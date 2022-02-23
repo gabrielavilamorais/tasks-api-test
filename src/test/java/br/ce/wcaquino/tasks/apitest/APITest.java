f66ea4c7fe320bebffbcb886e33eb6e043e9ee99
unknown <gabriel.avila@siriussoftware.com.br>
2022-02-23 11:27:51 -0300

    first commit

---
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -0,0 +1,59 @@
+package br.ce.wcaquino.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+
+public class APITest {
+	
+	@BeforeClass	
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8002/tasks-backend/todo";	
+		}
+
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given()
+		.when()
+			.get("/")
+		.then()
+			.statusCode(200)
+			;
+	}
+	
+	@Test
+	public void deveAdicionarTarefaComSucesso()	{
+		 String requestBody = "{ \"task\": \"tesete\", \"dueDate\": \"2022-12-30\" }";
+
+		RestAssured.given()
+            .header("Content-type", "application/json")
+            .and()
+            .body(requestBody)
+            .when()
+	            .post("/add")
+            .then()
+            	.statusCode(201)
+			;
+	}
+	
+	@Test
+	public void naoDeveAdicionarTarefaInvalida()	{
+		 String requestBody = "{ \"task\": \"tesete\", \"dueDate\": \"2010-12-30\" }";
+
+		RestAssured.given()
+            .header("Content-type", "application/json")
+            .and()
+            .body(requestBody)
+           .when()
+	            .post("/add")
+            .then()
+            	.log().all() 
+            	.statusCode(400)
+            	.body("message", CoreMatchers.is("Due date must not be in past"))
+			;
+	}
+	
+}
+
